ea273e532dcf458781a8ee7b0eb99a7ca91c7a5b
HeroicSam <cyuen10@gmail.com>
2023-02-02 18:58:33 -0500

    adding grass scene for tinkering

--- threejsexperiments/src/App.jsx
+++ threejsexperiments/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { Suspense, useState } from 'react'
+import { Canvas, useLoader } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 function App() {
 
-  return (
-    <div className="App">
+  const object = useLoader(GLTFLoader, 'grass.glb');
+  console.log(object);
 
-    </div>
+  return (
+    <Canvas camera={{near: 0.01, far: 110, fov: 40 }}>
+      <Suspense>
+        <primitive object={object.scene} />
+        <OrbitControls />
+        <Environment preset='apartment' />
+      </Suspense>
+    </Canvas>
   )
 }
 
